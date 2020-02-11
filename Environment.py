f252ca972b3b59649a46c1807e303f647fc1ccca
arthurgarnier <arthur.garnier5@gmail.com>
2020-02-11 15:58:56 +0100

    Envrironment coding of step and rating:

--- Environment.py
+++ Environment.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from copy import copy, deepcopy
 import random
 from scipy.stats import norm
+import networkx as nx
+from collections import defaultdict
+
 
 
 def random_argmax(rng, list_):
@@ -31,10 +34,10 @@ class ExplicitFeedback:
         #What about the left users?
         self.action_size = min(nb_users_men, nb_users_women)
         self.sampling_limit = nb_users_men * nb_users_women
-        self.user_mean = np.ones(self.internal_embedding_size)
-        self.user_var = np.ones(self.internal_embedding_size)
-        self.item_mean = np.ones(self.internal_embedding_size)
-        self.item_var = np.ones(self.internal)
+        self.men_mean = np.ones(self.internal_embedding_size)
+        self.men_var = np.ones(self.internal_embedding_size)
+        self.women_mean = np.ones(self.internal_embedding_size)
+        self.women_var = np.ones(self.internal)
         self.men_embedding = None
         self.women_embedding = None
         self.users_history = None
@@ -51,23 +54,52 @@ class ExplicitFeedback:
         self.action = action
         
         # compute potential rewards
-        potential_rewards = [self._get_user_item_rating(self.current_user, i) 
-                             for i in np.argwhere(self.user_item_history[self.current_user, :] == 0).flatten()]
-        
-        optimal_return = np.max(potential_rewards)
+        #potential_rewards = [self._get_user_item_rating(self.current_user, i) 
+        #                     for i in np.argwhere(self.user_item_history[self.current_user, :] == 0).flatten()]
+
+        potential_rewards = [[self._get_user_match(j,i) 
+                             for i in np.argwhere(self.user_match_history[j, :] == 0).flatten()] for j in self.nb_users_men]
+
+    	graph = nx.from_numpy_matrix(potential_rewards)
+    	pairs = graph.edges()
+
+
+    	#matching_pairs = [pairs[0]]
+    	#for p in pairs[1:]:
+    #		if p not in matching_pairs and len(matching_pairs)<nb_users_men: ## a voir si cardinaux egaux
+    		#	matching_pairs.append(p)
+
+    	#potential_max_match = [pairs[0]]
+    	#for i,j in pairs:
+    #		if ((i and j) not in potential_max_match[:,0]) and ((i and j) not in potential_max_match[:,1]):
+    #			potential_max_match.append((i,j))
+
+
+    	a = defaultdict(int)
+    	s=0
+   
+    	for i,j in pairs:
+    		a[i]+=1
+    	for key,d in a.items():
+    			s+=d-1
+
+        optimal_return = len(pairs)-s #self.action_size
 
         # map action to item
-        self.recommended_item = np.argwhere(self.user_item_history[self.current_user, :] == 0)[action][0]
+        for p in action:
+        	self.user_match_history[p[0],p[1]] =1 
+        	self.user_match_history[p[1],p[0]] =1 
+        #self.recommended_item = np.argwhere(self.user_match_history[self.current_user, :] == 0)[action][0]
 
         # mark item as rated
-        self.user_item_history[self.current_user, self.recommended_item] = 1
+        #self.user_match_history[self.current_user, self.recommended_item] = 1
 
         # compute reward R_t
-        self.current_rating = self._get_user_item_rating(self.current_user, self.recommended_item)
-        self.reward = self.current_rating
+        self.current_match = [self._get_user_item_rating(p[0],p[1]) for p in action]
+        self.reward = np.sum(self.current_match)
 
              # check if done
-        if self.user_item_history.sum() == self.sampling_limit:
+        if self.user_match_history.sum() == self.sampling_limit:
             self.done = True
 
         # compute next state S_{t+1}
@@ -80,37 +112,37 @@ class ExplicitFeedback:
 
     def reset(self, seed=None):
         self._rng = np.random.RandomState(seed)
-        self.action_size = self.nb_items
+        self.action_size = min(self.nb_users_men, self.nb_users_women)
         
         # create users and items embedding matrix
-        self.users_embedding = self._rng.normal(loc=self.user_mean,
-                                                scale=self.user_var,
-                                                size=(self.nb_users, self.internal_embedding_size))
-        self.items_embedding = self._rng.normal(loc=self.item_mean,
-                                                scale=self.item_var,
-                                                size=(self.nb_items, self.internal_embedding_size))
+        self.men_embedding = self._rng.normal(loc=self.men_mean,
+                                                scale=self.men_var,
+                                                size=(self.nb_users_men, self.internal_embedding_size))
+        self.women_embedding = self._rng.normal(loc=self.women_mean,
+                                                scale=self.women_var,
+                                                size=(self.nb_users_women, self.internal_embedding_size))
 
         # Let X = users_embedding and Y = items_embedding
         # In order to properly map float into integers, we need to know the distribution of
         # Z = \sum_k X_k.Y_k
         # E[Z] = \sum_k E[X_k.Y_k] = \sum_k E[X_k]E[Y_k]
         # Var[Z] = \sum_k Var[X_k.Y_k] = \sum_k Var[X_k]Var[Y_k] + Var[X_k]E[Y_k]^2 + Var[Y_k]E[X_k]^2
-        z_mean = self.user_mean.dot(self.item_mean)
-        z_var = self.user_var.dot(self.item_var) + self.user_var.dot(np.square(self.item_mean)) + \
-                self.item_var.dot(np.square(self.user_mean))
+        z_mean = self.men_mean.dot(self.women_mean)
+        z_var = self.men_var.dot(self.women_var) + self.men_var.dot(np.square(self.women_mean)) + \
+                self.women_var.dot(np.square(self.men_mean))
         z = norm(z_mean, np.sqrt(z_var))
         # to get 5 values, we need 4 cut points
-               self.z_cut_points = z.ppf([0.2, 0.4, 0.6, 0.8]) # you can control the distribution of ratings here.
-        self.user_item_history = np.zeros((self.nb_users, self.nb_items))
+               self.z_cut_points = z.ppf([0.8]) # you can control the distribution of ratings here.
+        self.user_match_history = np.zeros((self.nb_users_men, self.nb_users_women))
         self.done = False
 
         self._next_state()
         return self.state
 
-    def _get_user_item_rating(self, user, item):
-        real_score = self.users_embedding[user].dot(self.items_embedding[item])
-        integer_score = np.searchsorted(self.z_cut_points, real_score) + 1
-        return integer_score
+    def _get_user_match(self, user1, user2):
+        real_score = self.men_embedding[user1].dot(self.women_embedding[user2])
+        match_score = np.searchsorted(self.z_cut_points, real_score)
+        return match_score
 
     def _get_variables(self, user, item):
         user_embedding = self.users_embedding[user]
@@ -131,20 +163,20 @@ class ExplicitFeedback:
         for i in range(10):
             user = self._rng.randint(0, self.nb_users)
             # check it remain at least one item
-            if np.sum(self.user_item_history[user, :]) < self.nb_items:
+            if np.sum(self.user_match_history[user, :]) < self.nb_items:
                 return user
 
-        return self._rng.choice(np.argwhere(self.user_item_history == 0))[0]
+        return self._rng.choice(np.argwhere(self.user_match_history == 0))[0]
 
     def _next_state(self):
         # Pick a user
-        if self.user_item_history.sum() < self.sampling_limit:
+        if self.user_match_history.sum() < self.sampling_limit:
             self.current_user = self._get_new_user()
         else:
             self.current_user = None
 
         # List available items
-        self.available_items = np.argwhere(self.user_item_history[self.current_user, :] == 0)
+        self.available_items = np.argwhere(self.user_match_history[self.current_user, :] == 0)
 
         self.state = list()
         for i in self.available_items:
