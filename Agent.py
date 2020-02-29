06e0d63d6943f63b617dc80c3c2e7f257328bec9
arthurgarnier <arthur.garnier5@gmail.com>
2020-02-29 16:55:33 +0100

    add pure random agent in Agent.py

--- Agent.py
+++ Agent.py
@@ -1,14 +1,26 @@
-class Random:
+import numpy as np
+class PureRandom:
     """ Random agent. """
     def __init__(self, nb_arms, seed=None):
         self._nb_arms = nb_arms
         self._rng = np.random.RandomState(seed)
         
     def act(self, context):
-        action = self._rng.randint(len(context)) # note that action size is changing
+        minimum = np.argmin(np.array([len(context[0]),len(context[1])]))
+        order = np.random.choice(len(context[minimum]),len(context[minimum]),replace = False)
+        women=[]
+        nope = []
+        print(order)
+        for i in order:
+          b = context[2][i]
+          c = np.random.choice(b,1)
+          while c in nope:
+            c =np.random.choice(b,1,replace=False)
+            print(c)
+          nope.append(c)
+          women.append(c)
+        action = [(order[i],women[i][0]) for i in range(len(context[minimum]))]
         return action
         
     def update(self, context, action, reward):
         pass
-
-
