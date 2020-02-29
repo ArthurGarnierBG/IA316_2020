afd3f2b2aa28067dc24a7c1237e076401ce909af
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2020-02-29 15:28:25 +0100

    Update RunExp with plot and suits our problem

--- RunExp.py
+++ RunExp.py
@@ -1,42 +1,43 @@
+import matplotlib.pyplot as plt
+
 def run_exp(agent, env, nb_steps, env_seed):
     rewards = np.zeros(nb_steps)
     regrets = np.zeros(nb_steps)
-    actions = np.zeros(nb_steps)
-    context = env.reset(env_seed)
-    rating_matrix = np.zeros((env.nb_users, env.nb_items))
+
+    men_embedding, women_embedding, possible_recommendation = env.reset(env_seed)
+
     for i in range(nb_steps):
         # Select action from agent policy.
-        action = agent.act(context)
-        
+        recommendation = agent.act(men_embedding, women_embedding, possible_recommendation)
+
         # Play action in the environment and get reward.
-        reward, next_context, done, optimal_return = env.step(action)
-        
-        # Update history
-        user = context[0][0]
-        item = context[action][1]
-        rating = reward
-        rating_matrix[user, item] = rating
-        
+        reward, men_embedding, women_embedding, possible_recommendation, done, optimal_reward = env.step(recommendation)
+
         # Update agent.
-        agent.update(context, action, reward)
+        agent.update(reward)
         context = next_context
-        
+
         # Save history.
         #context[i] = context
         rewards[i] = reward
-        actions[i] = action
-        regrets[i] = optimal_return - reward
+        regrets[i] = optimal_reward - reward
 
     reward = rewards.sum()
     regret = np.sum(regrets)
-    return {'reward': reward, 
+
+
+    plt.plot(regrets.mean(axis=0), color='blue')
+    plt.plot(np.quantile(regrets, 0.05,axis=0), color='grey', alpha=0.5)
+    plt.plot(np.quantile(regrets, 0.95,axis=0), color='grey', alpha=0.5)
+    plt.title('Mean regret: {:.2f}'.format(regret.mean()))
+    plt.xlabel('steps')
+    plt.ylabel('regret')
+    plt.show()
+
+    return {'reward': reward,
             'regret': regret,
             'rewards': rewards,
             'regrets': regrets,
-            'actions': actions,
-            'cum_rewards': np.cumsum(rewards), 
+            'cum_rewards': np.cumsum(rewards),
             'cum_regrets': np.cumsum(regrets),
-            'rating_matrix': rating_matrix
             }
-
-
